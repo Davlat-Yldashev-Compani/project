9a5c626267a357e0401a23f656f2127e2b9f1eda
Davlat <tavlat2002@gmail.com>
2020-07-13 13:20:58 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,14 @@
+var incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+
+var isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose ); /*Оператор И*/
+console.log(isChecked || isClose ); /*Оператор ИЛИ*/
